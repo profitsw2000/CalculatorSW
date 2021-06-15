da80ccac20190dc0ea331dbb848b4d9b16583ea1
profitsw2000 <profitsw2000@gmail.com>
2021-06-15 19:46:16 +0300

    Сделано тестовое приложение, запускающее приложение-калькулятор.

--- app/src/main/java/ru/profitsw2000/calculatorsw/MainActivity.java
+++ app/src/main/java/ru/profitsw2000/calculatorsw/MainActivity.java
@@ -2,13 +2,40 @@ package ru.profitsw2000.calculatorsw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String EXT_LAUNCHER = "MyLauncher"    ;
+    Button button ;
+    TextView textView   ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button)  ;
+        textView = findViewById(R.id.text1) ;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("calculatorsw://intent") ;
+                Intent runIntent = new Intent(Intent.ACTION_VIEW, uri)  ;
+                ActivityInfo activityInfo = runIntent.resolveActivityInfo(getPackageManager(), runIntent.getFlags())    ;
+                if (activityInfo != null) {
+                    startActivity(runIntent);
+                }
+                else {
+                    textView.setText("Calculator was not found.");
+                }
+            }
+        });
     }
 }
